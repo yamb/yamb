a9268ef0178dd84930861bb3769bc8481bfe3722
Alexey Simonenko <dwarfman@gmail.com>
2013-12-18 20:04:12 +0400

    Add yapi option for tests

--- test/_data/options.js
+++ test/_data/options.js
@@ -1,4 +1,10 @@
-module.exports = {
+var options = {
   dbname: 'yamb-test',
   storage: require('./storage')
 };
+
+if (process.env.NODE_YAPI) {
+  options.yapi = process.env.NODE_YAPI;
+}
+
+module.exports = options;
