f7f1594380085f10ca3076a16571e4101604e359
Alexey Simonenko <dwarfman@gmail.com>
2013-12-31 19:22:27 +0400

    Update test data

--- test/_data/data.js
+++ test/_data/data.js
@@ -6,7 +6,11 @@ exports.create = {
   text: " I spend my days \"dashing\" from meeting to meeting, putting out fires, returning phone calls. I spend my meetings **reviewing designs**, **assessing concepts** and **directing my team**.  \n\n\n I spend my mornings reading the Wall Street Journal and my evenings watching the news.\nI'd say I'm *connected*. I'd say I'm successful. I'd say I'm spent!\n\nI drive to work. On a good day it's thirty minutes each way. On a bad day it's an hour and thirty minutes. If I leave early enough I can get there in twenty minutes (c) - now that's time management. \n\n  ",
   author: 'Alexey Simonenko',
   params: 'more parameters',
-  tags: 'business'
+  tags: 'business',
+  stats: {
+    views: '10',
+    unique: 5
+  }
 };
 
 exports.update = {
@@ -23,5 +27,10 @@ exports.update = {
   params: 'more parameters',
   tags: ['business'],
   created: '2013-12-23 18:59',
-  active: true
+  active: true,
+  stats: {
+    views: 10,
+    likes: 5,
+    comments: 20
+  }
 };
