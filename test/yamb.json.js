c9e224f49e47cc00371f70722c7b955de03aead2
Alexey Simonenko <dwarfman@gmail.com>
2014-02-19 19:17:20 +0400

    More tests for json()

--- test/yamb.json.js
+++ test/yamb.json.js
@@ -2,13 +2,36 @@ it('should works', function() {
   var a = new Yamb(data.create);
   var json = a.json();
 
+  json.id.should.equal(a.id);
+  json.title.should.equal(a.title);
+  json.text.should.equal(a.text);
+});
+
+it('should return only updated values', function() {
+  var a = new Yamb(data.create);
+  a.title = 'New title';
+
+  var json = a.json(true);
+
+  json.should.not.be.empty;
+  json.title.should.equal('New title');
+});
+
+it('should to be empty if nothing to change', function() {
+  var a = new Yamb(data.create);
+  var json = a.json(true);
+
+  json.should.be.empty;
+});
+
+it('should always clone returned object', function() {
+  var a = new Yamb(data.create);
+  var json = a.json();
+
   json.should.not.equal(a.json());
 
   json.title = 'Title';
   json.title.should.not.equal(a.title);
-
-  json.created.should.be.an.instanceof(Date);
-  json.created.toISOString.should.be.type('function');
 });
 
 it('should not have properties outside the schema', function() {
